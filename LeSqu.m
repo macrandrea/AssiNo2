29e7ccde96696c74b54f6406abd11c5dcac0c1c8
macrandrea <macri.andr@gmail.com>
2021-12-20 01:34:04 +0100

    alcuni algoritmi -Chol non era necessario ma fatto uguale

--- LeSqu.m
+++ LeSqu.m
@@ -1,15 +1,17 @@
-function xyz=LeSqu(x,y,polyMax)
+function x=LeSqu(x,y,polyMax)
 A=Vander(x,polyMax);
 C=A'*A;
 d=A'*y;
 m=width(A);
 n=rank(A);
-if m==n %check full cols rank
-    choleski Cx=d
-    b=x'*A*x;
-else
-    error('No full cols rank')
-end
-
-
+    if m==n %check full cols rank
+        L=chol(C);
+        x=inv(L'*L)*L'*d;
+        %x=inv(L)*d;
+        %x=inv(L)*d;
+        %choleski Cx=d
+        %b=x'*A*x;
+    else
+        error('No full cols rank')
+    end
 end
