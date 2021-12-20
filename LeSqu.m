23455554e497737c3df7aad834246b285e431398
macrandrea <macri.andr@gmail.com>
2021-12-20 14:26:31 +0100

    ora funziona

--- LeSqu.m
+++ LeSqu.m
@@ -4,12 +4,22 @@ C=A'*A;
 d=A'*y;
 m=length(d);
 n=rank(A);
+L=chol(C,"lower");
+R=chol(C,"upper");
     if m==n %check full cols rank
-        L=chol(C,"upper");
-        u=backsubUTri(L,d,m);
-        R=chol(C,"lower");%forse inefficiente
-        ell=backsubLoTri(R,d,m);
+        ell=backsubLoTri(L,d,m);
+        u=backsubUTri(R,ell,m);
     else
         error('No full cols rank')
     end
 end
+        %u=L\d;
+        %ell=L'\u;
+        %L=chol(C,"upper");
+        %u=
+        %R=chol(C,"lower");%forse inefficiente
+        %
+
+
+%L*y=d
+%U*x=y
